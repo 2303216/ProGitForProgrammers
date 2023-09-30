d01abf6ec862eb10483a98e399916879235eb5ae
2303216 <145557049+2303216@users.noreply.github.com>
2023-09-30 22:22:16 +0100

    Created new project in VS

---
+++ ProGitForProgrammers/ProGitForProgrammers/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PrpGitForProgrammers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

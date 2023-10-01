1cb6807b37c7abb5ed0d1a006ef5b4274604ca25
2303216 <145557049+2303216@users.noreply.github.com>
2023-10-01 18:22:59 +0100

    new line of code

--- ProGitForProgrammers/ProGitForProgrammers/Program.cs
+++ ProGitForProgrammers/ProGitForProgrammers/Program.cs
@@ -7,6 +7,7 @@ namespace PrpGitForProgrammers
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("New line of code!");
         }
     }
 }

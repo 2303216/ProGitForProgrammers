946dc6626fe145e2ebfb5fe3e7afde422cc90e67
2303216 <145557049+2303216@users.noreply.github.com>
2023-10-05 10:04:41 +0100

    Add a book classadn properties

---
+++ ProGitForProgrammers/ProGitForProgrammers/Book.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProGitForProgrammers
+{
+    public class Book
+    {
+        public String Title { get; set; }
+        public List<string> Authors { get; set; }
+        public DateTime PublicatioDate { get; set; }
+    }
+}
